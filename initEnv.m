7f0c521a64380cd7ef97a45635b37cb829edd742
Ceren <battal.ceren@gmail.com>
2020-10-02 15:58:52 +0200

    env update

--- initEnv.m
+++ initEnv.m
@@ -103,5 +103,6 @@ function addDependencies()
     addpath(genpath(fullfile(pth, 'lib', 'CPP_BIDS_SPM_pipeline', 'src')));
     addpath(genpath(fullfile(pth, 'lib', 'CPP_BIDS_SPM_pipeline', 'lib')));
     addpath(fullfile(pth, 'src'));
+    addpath(genpath('/Users/battal/Documents/MATLAB/spm12'));
 
 end
--- src/rhythmCategBlock.m
+++ src/rhythmCategBlock.m
@@ -12,14 +12,19 @@ addpath(fullfile(fileparts(mfilename('fullpath')), '..'));
 initEnv();
 
 % we add all the subfunctions that are in the sub directories
-opt = getOptionVisualLoc();
+opt = getOptionCategBlock();
 
 checkDependencies();
 
 %% Run batches
 
-% bidsCopyRawFolder(opt, 1);
-bidsRealignReslice(opt);
+%bidsCopyRawFolder(opt, 0);
+bidsSTC(opt);
+% BIDS_SpatialPrepro(opt);
+% BIDS_Smoothing(6, opt);
+
+
+% bidsRealignReslice(opt);
 % bidsSmoothing(FWHM, opt);
 % bidsFFX('specifyAndEstimate', opt, FWHM, isMVPA);
 % bidsFFX('contrasts', opt, FWHM, isMVPA);
