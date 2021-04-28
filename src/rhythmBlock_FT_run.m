90b053ba1d6ba536fe9250e9ae38c80577c1e293
Ceren <battal.ceren@gmail.com>
2021-04-28 17:08:30 +0200

    FT path set

--- src/rhythmBlock_FT_run.m
+++ src/rhythmBlock_FT_run.m
@@ -7,17 +7,17 @@ addpath(pth);
 % add FFT analysis lib
 addpath(genpath(fullfile(pth, 'lib', 'FFT_fMRI_analysis')));
 
-  %% set paths
-  % set spm
-  [~, hostname] = system('hostname');
+%% set paths
+% set spm
+[~, hostname] = system('hostname');
+warning('off');
+
+if strcmp(deblank(hostname), 'tux')
+  addpath(genpath('/home/tomo/Documents/MATLAB/spm12'));
+elseif strcmp(deblank(hostname), 'mac-114-168.local')
   warning('off');
-  
-  if strcmp(deblank(hostname), 'tux')
-    addpath(genpath('/home/tomo/Documents/MATLAB/spm12'));
-  elseif strcmp(deblank(hostname), 'mac-114-168.local')
-    warning('off');
-    addpath(genpath('/Users/battal/Documents/MATLAB/spm12'));
-  end
+  addpath(genpath('/Users/battal/Documents/MATLAB/spm12'));
+end
 
 % add cpp repo
 run ../lib/CPP_BIDS_SPM_pipeline/initCppSpm.m;
@@ -30,19 +30,16 @@ opt = getOptionBlock();
 opt.anatMask = 0;
 opt.FWHM = 0; % 3 or 6mm smoothing
 
-
-
 % create a whole brain functional mean image mask
 % so the mask will be in the same resolution/space as the functional images
 % one may not need it if they are running bidsFFX since it creates a
 % mask.nii by default
-% opt.skullstrip.threshold = 0.5;
+% opt.skullstrip.threshold = 0.5; -->provides bigger mask thn default value
 opt.skullstrip.mean = 1;
 opt.funcMask = bidsWholeBrainFuncMask(opt);
 
-
 % want to save each run FFT results
-opt.saveEachRun = 0; 
+opt.saveEachRun = 0;
 % step size
 opt.nStepsPerPeriod = 2;
 % run fft
